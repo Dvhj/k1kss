2b7f1029345dcb62975975753c2144de1dcc98ed
Dvhj <kirillov97@list.ru>
2022-07-06 09:21:38 +0400

    add anime in stat sqreen

---
+++ app2.js
@@ -0,0 +1,42 @@
+const startAnim = () => {
+	const start = document.querySelector('.start')
+	const startTitle = document.querySelector('.start_title')
+	let count = 1
+	let x
+	// x  = setInterval(() => {
+	// 	count-=0.01
+	// 	start.style.background = `rgba(28, 27, 33, ${count})`;
+	// 	console.log(start.style.background)
+	// 	if ( count <= 0.3) {
+	// 		clearInterval(x)
+	// 	}
+	// },100)
+
+
+	let a = startTitle.textContent.split('')
+	// a.reverse()
+	startTitle.textContent = ''
+	for ( let i = 0; i < a.length; i++) {
+		setTimeout(()=> {
+			let newg = document.createElement('span')
+			newg.textContent = `${a[i]}`
+			startTitle.appendChild(newg)
+		}, i*200)
+	}
+
+
+
+
+
+
+
+
+
+
+	setTimeout(() => {
+		start.style.transform= 'translateX(-100vw)';
+		document.body.style.overflowY = 'scroll'
+	}, 3000)
+}
+
+startAnim();
